fa572e696c2b60a7f375faeb4074aa7a4b2214d0
shawnwongsit <2101229@sit.singaporetech.edu.sg>
2022-10-02 17:18:08 +0800

    Tidy up code

--- hide_img_in_text_file.py
+++ hide_img_in_text_file.py
@@ -41,187 +41,7 @@ def get_n_most_significant_bits(value, n):
 def shift_n_bits_to_8(value, n):
     return value << MAX_BIT_VALUE - n
 
-
-# Function to encode the image that we want to hide, into the cover image
-def encodeImage(image_to_hide, file_to_hide_in, n_bits):
-    # Copy contents in file_to_hide_in to the encoded txt file
-    """file_hide = open(file_to_hide_in, "r")
-    f = open("encoded_file.txt", "w")
-    f.write(file_hide.read())
-    f.close()"""
-
-    # Word doc part
-    # file_hide = docx2txt.process(file_to_hide_in)
-    # print(file_hide)
-    # f = open("encoded_file.docx", "w")
-    # f.write(file_hide)
-    """document = docx.Document(file_to_hide_in)
-    allText = []
-    for docpara in document.paragraphs:
-        allText.append(docpara.text)
-    print(allText)"""
-
-    # List to store the ascii values from the content of the file
-    file_bytes = []
-
-    with open(file_to_hide_in, "rb") as out:
-        while True:
-            c = out.read(1)
-            if not c:
-                print("End of file")
-                break
-            file_bytes.append(ord(c))
-
-    # total = len(file_bytes) % 3
-    # print(file_bytes)
-    # a,b,c = file_bytes[0],file_bytes[1],file_bytes[2]
-    # print(a," ", b, " ",c)
-
-    # Check size of cover file
-    file_size = os.path.getsize(file_to_hide_in)  # file_size is the same as len(file_bytes)
-    print("The txt file size is ", file_size)
-
-    # Keep adding character till it can be reshaped into a list with 3 elements (In the form of RGB (1,2,3))
-    while (len(file_bytes) % 3 != 0):
-        file_bytes.append(ord("A"))
-        # print(file_size % 3)
-
-    file_bytes = np.array(file_bytes)
-    file_bytes = np.reshape(file_bytes, (-1, 3))
-
-    # Check size of payload img
-    width = image_to_hide.size[0]
-    height = image_to_hide.size[1]
-    print("SIZE OF IMG ", image_to_hide.size)
-    img_size = (width * height) * 3  # multiply by 3 cuz there's 3 bytes per pixel
-
-    # Check if cover file size is smaller than payload img size
-    if (file_size < img_size):
-        print("Cover object size is smaller than payload size")
-        quit()
-
-    # print(len(file_bytes))
-
-    # print("TEST ", file_bytes[0])
-    """for i in file_bytes:
-        print("TEST ", file_bytes[i])"""
-
-    # .load() returns the "pixel_access" object that has the data(matrix) of the pixels.
-    hide_image = image_to_hide.load()
-    # print("HIDE IMAGE ", hide_image[0,0])
-
-    # this will store the values of each individual pixel as a matrix.
-    data = []
-    counter = 0  # Loop thru the txt file
-    # looping the hide_image object.
-    print("HEIGHT ", height)
-    print("WIDTH ", width)
-    for y in range(height):
-        for x in range(width):
-            # print(counter)
-
-            # most sig bits
-            # print(hide_image[x,y]) #Uncomment this to see the pixel values in r,g,b form. (Shows every pixel's RGB value)
-            try:
-                # the value of n can be 1 or 2 and you won't see much difference in the encoded image.
-                # gets n most significant bits of r,g,b values of image to hide.
-                # print("HIDE IMAGE ", hide_image[x,y])
-                r_hide, g_hide, b_hide = hide_image[x, y]
-
-                r_hide = get_n_most_significant_bits(r_hide, n_bits)
-                g_hide = get_n_most_significant_bits(g_hide, n_bits)
-                b_hide = get_n_most_significant_bits(b_hide, n_bits)
-
-                # remove least n significant bits of txt file's content to hide in so we can store
-                # the n most significant bits in that place.
-
-                r_hide_in, g_hide_in, b_hide_in = file_bytes[counter]
-
-                r_hide_in = remove_n_least_significant_bits(r_hide_in, n_bits)
-                g_hide_in = remove_n_least_significant_bits(g_hide_in, n_bits)
-                b_hide_in = remove_n_least_significant_bits(b_hide_in, n_bits)
-
-                # Adding each MSB R,G,B from payload to LSB R,G,B cover image
-
-                data.append((r_hide + r_hide_in,
-                             g_hide + g_hide_in,
-                             b_hide + b_hide_in))
-                counter += 1
-
-            # incase of exception it will show the reason.
-            except Exception as e:
-                print(e)
-    # print("Data ", data)
-    data = np.array(data)
-    data = data.flatten()
-
-    # Writing encoded characters to encoded_file.txt
-    # charData = []
-    with open("encoded_file.txt", "w", encoding="utf-8") as f:
-        for i in data:
-            f.write(chr(i))
-            # charData.append(chr(i))
-        print(str(height), " ", str(width))
-        f.write("=====")
-        f.write("HEIGHT=")
-        f.write(str(height))
-        f.write("WIDTH=")
-        f.write(str(width))
-
-
-    #WRITING TO WORD DOC. NOT WORKING    
-    """textList = []
-    decoded_doc = docx.Document()
-    for i in data:
-        #print(chr(i))
-        x = chr(i)
-        #print(x)
-        cleaned_string = ''.join(c for c in x if valid_xml_char_ordinal(c))
-        #print(cleaned_string)
-        textList.append(cleaned_string)
-    
-    fullText = ""
-    for i in textList:
-        fullText += i
-    #print(fullText)
-    fullText += ("=====")
-    fullText += ("HEIGHT=")
-    fullText += (str(height))
-    fullText += ("WIDTH=")
-    fullText += (str(width))
-    decoded_doc.add_paragraph(fullText)
-    decoded_doc.save("encoded_file.docx")"""
-
-    """for i in range(len(charData)):
-        charData[i] = ord(charData[i])"""
-    # print("Char Data ", charData)
-
-    # return an height and width from the above data.
-    return height, width
-
-
-# Decode image and n_bits as parameters.
-def decodeFile(file_to_decode, n_bits):
-    # List to store the ascii values from the content of the file
-    asciiArray = []
-    with open(file_to_decode, "r", encoding="utf-8") as f:
-        for i in f.read():
-            #print(i)
-            asciiArray.append(ord(i))
-
-    #READ WORD DOC. NOT WORKING
-    """fullText = []
-    doc = docx.Document(file_to_decode)
-    for i in doc.paragraphs:
-        #print(i.text)
-        fullText.append(i.text)
-    docText = '\n'.join(fullText)
-    for i in docText:
-        #print(ord(i))
-        asciiArray.append(ord(i))"""
-    
-    print("TEST ", len(asciiArray))
-    #print("ASD ", asciiArray[-20:])
+def getHeightWidth(asciiArray):
     counter = 0
     index = 0
     heightWidth = None
@@ -270,25 +90,168 @@ def decodeFile(file_to_decode, n_bits):
         height += i
     for i in widthArray:
         width += i
+
+    return height,width,newAsciiArray
+
+
+# Function to encode the image that we want to hide, into the cover image
+def encodeImage(image_to_hide, file_to_hide_in, n_bits, outputFile):
+
+    # List to store the ascii values from the content of the file
+    file_bytes = []
+
+    with open(file_to_hide_in, "rb") as out:
+        while True:
+            c = out.read(1)
+            if not c:
+                print("End of file")
+                break
+            file_bytes.append(ord(c))
+
+    # Check size of cover file
+    file_size = os.path.getsize(file_to_hide_in)  # file_size is the same as len(file_bytes)
+    print("The txt file size is ", file_size)
+
+    # Keep adding character till it can be reshaped into a list with 3 elements (In the form of RGB (1,2,3))
+    while (len(file_bytes) % 3 != 0):
+        file_bytes.append(ord("A"))
+
+    file_bytes = np.array(file_bytes)
+    file_bytes = np.reshape(file_bytes, (-1, 3))
+
+    # Check size of payload img
+    width = image_to_hide.size[0]
+    height = image_to_hide.size[1]
+    print("SIZE OF IMG ", image_to_hide.size)
+    img_size = (width * height) * 3  # multiply by 3 cuz there's 3 bytes per pixel
+
+    # Check if cover file size is smaller than payload img size
+    if (file_size < img_size):
+        print("Cover object size is smaller than payload size")
+        quit()
+
+    # .load() returns the "pixel_access" object that has the data(matrix) of the pixels.
+    hide_image = image_to_hide.load()
+
+    # this will store the values of each individual pixel as a matrix.
+    data = []
+    counter = 0  # Loop thru the txt file
+    # looping the hide_image object.
+    print("HEIGHT ", height)
+    print("WIDTH ", width)
+    for y in range(height):
+        for x in range(width):
+            try:
+                # gets n most significant bits of r,g,b values of image to hide.
+                # print("HIDE IMAGE ", hide_image[x,y])
+                r_hide, g_hide, b_hide = hide_image[x, y]
+
+                r_hide = get_n_most_significant_bits(r_hide, n_bits)
+                g_hide = get_n_most_significant_bits(g_hide, n_bits)
+                b_hide = get_n_most_significant_bits(b_hide, n_bits)
+
+                # remove least n significant bits of txt file's content to hide in so we can store
+                # the n most significant bits in that place.
+
+                r_hide_in, g_hide_in, b_hide_in = file_bytes[counter]
+
+                r_hide_in = remove_n_least_significant_bits(r_hide_in, n_bits)
+                g_hide_in = remove_n_least_significant_bits(g_hide_in, n_bits)
+                b_hide_in = remove_n_least_significant_bits(b_hide_in, n_bits)
+
+                # Adding each MSB R,G,B from payload to LSB R,G,B cover image
+
+                data.append((r_hide + r_hide_in,
+                             g_hide + g_hide_in,
+                             b_hide + b_hide_in))
+                counter += 1
+
+            # incase of exception it will show the reason.
+            except Exception as e:
+                print(e)
+
+    # print("Data ", data)
+    data = np.array(data)
+    data = data.flatten()
+
+    # Writing encoded characters to encoded_file.txt
+    with open(outputFile, "w", encoding="utf-8") as f:
+        for i in data:
+            f.write(chr(i))
+            # charData.append(chr(i))
+        print(str(height), " ", str(width))
+        f.write("=====")
+        f.write("HEIGHT=")
+        f.write(str(height))
+        f.write("WIDTH=")
+        f.write(str(width))
+
+
+    #WRITING TO WORD DOC. NOT WORKING    
+    """textList = []
+    decoded_doc = docx.Document()
+    for i in data:
+        #print(chr(i))
+        x = chr(i)
+        #print(x)
+        cleaned_string = ''.join(c for c in x if valid_xml_char_ordinal(c))
+        #print(cleaned_string)
+        textList.append(cleaned_string)
+    
+    fullText = ""
+    for i in textList:
+        fullText += i
+    #print(fullText)
+    fullText += ("=====")
+    fullText += ("HEIGHT=")
+    fullText += (str(height))
+    fullText += ("WIDTH=")
+    fullText += (str(width))
+    decoded_doc.add_paragraph(fullText)
+    decoded_doc.save("encoded_file.docx")"""
+
+    # return an height and width from the above data.
+    return height, width
+
+
+# Decode image and n_bits as parameters.
+def decodeFile(file_to_decode, n_bits):
+    # List to store the ascii values from the content of the file
+    asciiArray = []
+    with open(file_to_decode, "r", encoding="utf-8") as f:
+        for i in f.read():
+            #print(i)
+            asciiArray.append(ord(i))
+
+    #READ WORD DOC. NOT WORKING
+    """fullText = []
+    doc = docx.Document(file_to_decode)
+    for i in doc.paragraphs:
+        #print(i.text)
+        fullText.append(i.text)
+    docText = '\n'.join(fullText)
+    for i in docText:
+        #print(ord(i))
+        asciiArray.append(ord(i))"""
+    
+    #print("TEST ", len(asciiArray))
+
+    height,width,newAsciiArray = getHeightWidth(asciiArray)
     
     height = int(height)
     width = int(width)
     print(height, " ", width)
 
-    """asciiSize = len(newAsciiArray) / 3
-    while (asciiSize < (height * width)):
-        #print("HELLO")
-        newAsciiArray.append(ord("A"))
-        asciiSize = len(newAsciiArray) / 3"""
+
     # Keep adding character till it can be reshaped into a list with 3 elements (In the form of RGB (1,2,3))
     while (len(newAsciiArray) % 3 != 0):
         newAsciiArray.append(ord("A"))
         # print(file_size % 3)
     
-    print(len(newAsciiArray))
+    #print(len(newAsciiArray))
     npAsciiArray = np.array(newAsciiArray)
     npAsciiArray = np.reshape(npAsciiArray, (-1, 3))
-    print("ASCIITEST ", len(npAsciiArray))
+    #print("ASCIITEST ", len(npAsciiArray))
 
     # matrix that will store the extracted pixel values from the encoded txt file.
     data = []
@@ -329,7 +292,7 @@ image_to_hide_in_path = "test.txt"
 #image_to_hide_in_path = "wordTest.docx"
 #image_to_hide_in_path = "excel.xlsx"
 image_to_hide = Image.open(image_to_hide_path)
-encodeImage(image_to_hide, image_to_hide_in_path, n_bits)
+encodeImage(image_to_hide, image_to_hide_in_path, n_bits, "encoded_file.txt")
 print("File encoded Successfully!")
 #
 # # running the decode function
--- hide_text_file_in_img.py
+++ hide_text_file_in_img.py
@@ -2,7 +2,6 @@ from PIL import Image
 import os, numpy as np
 import docx2txt
 import docx
-import aspose.words as aw
 
 MAX_COLOR_VALUE = 255
 MAX_BIT_VALUE = 8
@@ -53,38 +52,70 @@ def get_n_most_significant_bits(value, n):
 def shift_n_bits_to_8(value, n):
     return value << MAX_BIT_VALUE - n
 
-
-# Function to encode the image that we want to hide, into the cover image
-def encodeTextFile(file_to_hide, image_to_hide_in, n_bits):
-    width = image_to_hide_in.size[0]
-    height = image_to_hide_in.size[1]
-    hide_in_image = image_to_hide_in.load()
-
-
-    # List to store the ascii values from the content of the file
-    file_bytes = []
-
-    #Uncomment to encode txt file in img
-    with open(file_to_hide, "rb") as out:
+def asciiListForTxtFile(filename, list):
+    with open(filename, "rb") as out:
         while True:
             c = out.read(1)
-            #print(c)
             if not c:
                 print("End of file")
                 break
-            file_bytes.append(ord(c))
+            list.append(ord(c))
+    #print(list)
 
-    #Uncomment to encode docx file in img
-    """fullText = []
-    doc = docx.Document(file_to_hide)
+def asciiListForDocxFile(filename, list):
+    fullText = []
+    doc = docx.Document(filename)
     for i in doc.paragraphs:
         #print(i.text)
         fullText.append(i.text)
     docText = '\n'.join(fullText)
     for i in docText:
         #print(ord(i))
-        file_bytes.append(ord(i))"""
+        list.append(ord(i))
 
+def retrieveTxtFileFromImg(data):
+    with open("decoded_file.txt", "w", encoding="utf-8") as f:
+        for i in data:
+            f.write(chr(i))
+
+def retrieveDocxFileFromImg(data):
+    textList = []
+    decoded_doc = docx.Document()
+    for i in data:
+        x = chr(i)
+        #print(x)
+        cleaned_string = ''.join(c for c in x if valid_xml_char_ordinal(c))
+        #print(cleaned_string)
+        textList.append(cleaned_string)
+    
+    fullText = ""
+    for i in textList:
+        fullText += i
+    #print(fullText)
+    decoded_doc.add_paragraph(fullText)
+    decoded_doc.save("decoded_docx.docx")
+
+# Function to encode the document file (txt,docx) that we want to hide, into a cover image
+def encodeTextFile(file_to_hide, image_to_hide_in, n_bits):
+    #Getting width and height of cover image
+    width = image_to_hide_in.size[0]
+    height = image_to_hide_in.size[1]
+
+    #.load() returns the "pixel_access" object that has the data(matrix) of the pixels.
+    hide_in_image = image_to_hide_in.load()
+
+    #Check payload file type
+    toHide_file_type = file_to_hide.split(".")[1]
+
+    # List to store the ascii values from the content of the file
+    file_bytes = []
+
+    if(toHide_file_type == "txt"):
+        #Uncomment to retrieve txt file text
+        asciiListForTxtFile(file_to_hide, file_bytes)
+    elif(toHide_file_type == "docx"):
+        #Uncomment to retrieve docx text
+        asciiListForDocxFile(file_to_hide, file_bytes)
 
     # Check size of cover img
     img_size = (width * height) * 3
@@ -93,13 +124,10 @@ def encodeTextFile(file_to_hide, image_to_hide_in, n_bits):
     # Keep adding character till it can be reshaped into a list with 3 elements (In the form of RGB (1,2,3))
     while (len(file_bytes) % 3 != 0):
         file_bytes.append(ord("A"))
-        # print(file_size % 3)
 
     file_bytes = np.array(file_bytes)
     file_bytes = np.reshape(file_bytes, (-1, 3))
-    print(len(file_bytes))
-    """for i in file_bytes:
-        print(i)"""
+    #print(len(file_bytes))
 
     file_size = os.path.getsize(file_to_hide)  # file_size is the same as len(file_bytes)
     print("The payload file size is ", file_size)
@@ -115,18 +143,12 @@ def encodeTextFile(file_to_hide, image_to_hide_in, n_bits):
     # looping the hide_image object.
     print("HEIGHT ", height)
     print("WIDTH ", width)
-    for y in range(height):
-        
-        for x in range(width):
-            
-            # print(counter)
 
-            # most sig bits
-            # print(hide_image[x,y]) #Uncomment this to see the pixel values in r,g,b form. (Shows every pixel's RGB value)
+    for y in range(height):
+        for x in range(width):
+
             try:
-                # the value of n can be 1 or 2 and you won't see much difference in the encoded image.
-                # gets n most significant bits of r,g,b values of image to hide.
-                # print("HIDE IMAGE ", hide_image[x,y])
+                #Get every MSB ASCII value of payload file
                 if (counter < len(file_bytes)):
                     r_hide, g_hide, b_hide = file_bytes[counter]
                     #print(r_hide," ", g_hide," ", b_hide)
@@ -167,7 +189,8 @@ def encodeTextFile(file_to_hide, image_to_hide_in, n_bits):
     return createImage(data, image_to_hide_in.size)
 
 #Decode image and n_bits as parameters.
-def decodeImage(image_to_decode, n_bits):
+def decodeImage(image_to_decode, n_bits, fileType):
+    #Encoded Image width and height
     width = image_to_decode.size[0]
     height = image_to_decode.size[1]
     encoded_image = image_to_decode.load()
@@ -175,7 +198,7 @@ def decodeImage(image_to_decode, n_bits):
     #matrix that will store the extracted pixel values from the encoded Image.
     data = []
 
-    counter = 0
+    #counter = 0
     
     #looping through every pixel in the encoded Image.
     for y in range(height):
@@ -198,109 +221,39 @@ def decodeImage(image_to_decode, n_bits):
             b_encoded = shift_n_bits_to_8(b_encoded, n_bits)
 
             data.append((r_encoded, g_encoded, b_encoded))
-            counter += 1
-            """if (counter == 12666):
-                break"""
+            #counter += 1
             
     data = np.array(data)
     data = data.flatten()
     print(data)
 
-    #Uncomment to retrieve txt file from image
-    with open("decoded_file.txt", "w", encoding="utf-8") as f:
-        for i in data:
-            f.write(chr(i))
+    if(fileType == "txt"):
+        #Uncomment to retrieve txt file from image
+        retrieveTxtFileFromImg(data)
+    elif(fileType == "docx"):
+        #Uncomment to retrieve docx file from image
+        retrieveDocxFileFromImg(data)
 
-    #Uncomment to retrieve docx file from image
-    """textList = []
-    decoded_doc = docx.Document()
-    #with open("decoded_docx.docx", "w", encoding="utf-8") as f:
-    for i in data:
-        #print(chr(i))
-        x = chr(i)
-        #print(x)
-        cleaned_string = ''.join(c for c in x if valid_xml_char_ordinal(c))
-        #print(cleaned_string)
-        textList.append(cleaned_string)
-    
-    fullText = ""
-    for i in textList:
-        fullText += i
-    #print(fullText)
-    decoded_doc.add_paragraph(fullText)
-    decoded_doc.save("decoded_docx.docx")"""
-
-    
     
     return None
-    return createImage(data, image_to_decode.size)
-
-# Decode image and n_bits as parameters.
-def decodeFile(file_to_decode, n_bits, height, width):
-    
-    # List to store the ascii values from the content of the file
-    asciiArray = []
-    with open(file_to_decode, "r", encoding="utf-8") as f:
-        for i in f.read():
-            asciiArray.append(ord(i))
-    # Keep adding character till it can be reshaped into a list with 3 elements (In the form of RGB (1,2,3))
-    while (len(asciiArray) % 3 != 0):
-        asciiArray.append(ord("A"))
-        # print(file_size % 3)
-    npAsciiArray = np.array(asciiArray)
-    npAsciiArray = np.reshape(npAsciiArray, (-1, 3))
-    # print("ASCIITEST ", npAsciiArray)
-
-    # matrix that will store the extracted pixel values from the encoded txt file.
-    data = []
-    counter = 0
-    # looping through every pixel in the encoded Image.
-    for y in range(height):
-        for x in range(width):
-            # gets rgb values of encoded txt file.
-            r_encoded, g_encoded, b_encoded = npAsciiArray[counter]
-
-            # get n least significant bits for each r,g,b value of the encoded image
-            r_encoded = get_n_least_significant_bits(r_encoded, n_bits)
-            g_encoded = get_n_least_significant_bits(g_encoded, n_bits)
-            b_encoded = get_n_least_significant_bits(b_encoded, n_bits)
-
-            # shifts the n bits to right so that they occupy a total of 8 bit spaces.
-            # If 10 are the bits then shifting them would look like 10000000
-            # this would ofcourse be converted to an int as per python's bit operations.
-
-            r_encoded = shift_n_bits_to_8(r_encoded, n_bits)
-            g_encoded = shift_n_bits_to_8(g_encoded, n_bits)
-            b_encoded = shift_n_bits_to_8(b_encoded, n_bits)
-
-            data.append((r_encoded, g_encoded, b_encoded))
-            counter += 1
-    # print("DATA ", data)
-
-    return createImage(data, (width, height))
-
 
 # # Running encode function
 # # no. of bits
-n_bits = 8
+n_bits = 7
 # # path of files replace them as per your need.
-#image_to_hide_path = "tree2.jpg"
-#image_to_hide_in_path = "test.txt"
-#image_to_hide_in_path = "wordTest.docx"
-#image_to_hide_in_path = "excel.xlsx"
 file_to_hide_path = "test.txt"
 cover_img_path = "pics/tree.jpg"
 image_to_hide = Image.open(cover_img_path)
 #height, width = encodeImage(image_to_hide, image_to_hide_in_path, n_bits)
 encoded_image_path = "pics/encoded_img_from_file.png"
-encodeTextFile(file_to_hide_path, image_to_hide, n_bits).save(encoded_image_path)
-print("File encoded Successfully!")
+#encodeTextFile(file_to_hide_path, image_to_hide, n_bits).save(encoded_image_path)
+#print("File encoded Successfully!")
 #
 # # running the decode function
-n_bits = 8
+n_bits = 7
 #
 # # path where you would want to save decoded Image.
-image_to_decode_path = "pics/encoded_img_from_docx.png"
+image_to_decode_path = "pics/encoded_img_from_file.png"
 image_to_decode = Image.open(image_to_decode_path)
-decodeImage(image_to_decode, n_bits)
+decodeImage(image_to_decode, n_bits, file_to_hide_path.split(".")[1])
 print("Image decoded Successfully!")
