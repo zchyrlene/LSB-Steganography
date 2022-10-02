07bf5b03bac121edac8563c91728026af3478484
shawnwongsit <2101229@sit.singaporetech.edu.sg>
2022-10-02 15:03:42 +0800

    Added docx to txt

--- text_file_in_text_file.py
+++ text_file_in_text_file.py
@@ -53,33 +53,53 @@ def get_n_most_significant_bits(value, n):
 def shift_n_bits_to_8(value, n):
     return value << MAX_BIT_VALUE - n
 
+def saveDocxFile(data, filename):
+    textList = ""
+    encoded_doc = docx.Document()
+    for i in data:
+        textList += i
+    
+    #print(textList)
+    cleaned_string = ''.join(c for c in textList if valid_xml_char_ordinal(c))
+    encoded_doc.add_paragraph(cleaned_string)
+    encoded_doc.save(filename)
+
+def asciiListForTxtFile(filename, list):
+    with open(filename, "rb") as out:
+        while True:
+            c = out.read(1)
+            if not c:
+                print("End of file")
+                break
+            list.append(ord(c))
+    print(list)
+
+def asciiListForDocxFile(filename, list):
+    fullText = []
+    doc = docx.Document(filename)
+    for i in doc.paragraphs:
+        #print(i.text)
+        fullText.append(i.text)
+    docText = '\n'.join(fullText)
+    for i in docText:
+        #print(ord(i))
+        list.append(ord(i))
 
 # Function to encode the image that we want to hide, into the cover image
 def encodeTextFile(file_to_hide, file_to_hide_in, n_bits):
-
-
+    toHide_file_type = file_to_hide.split(".")[1]
+    print(toHide_file_type)
+    
+    #Opening of payload file
     # List to store the ascii values from the content of the file
     payload_bytes = []
 
-    with open(file_to_hide, "rb") as out:
-        while True:
-            c = out.read(1)
-            if not c:
-                print("End of file")
-                break
-            payload_bytes.append(ord(c))
-    #print(payload_bytes)
-
-    cover_bytes = []
-
-    with open(file_to_hide_in, "rb") as out:
-        while True:
-            c = out.read(1)
-            if not c:
-                print("End of file")
-                break
-            cover_bytes.append(ord(c))
-    print(cover_bytes)
+    if(toHide_file_type == "txt"):
+        #Uncomment to retrieve txt file text
+        asciiListForTxtFile(file_to_hide, payload_bytes)
+    elif(toHide_file_type == "docx"):
+        #Uncomment to retrieve docx text
+        asciiListForDocxFile(file_to_hide, payload_bytes)
 
     """fullText = []
     doc = docx.Document(file_to_hide)
@@ -89,7 +109,21 @@ def encodeTextFile(file_to_hide, file_to_hide_in, n_bits):
     docText = '\n'.join(fullText)
     for i in docText:
         #print(ord(i))
-        file_bytes.append(ord(i))"""
+        payload_bytes.append(ord(i))"""
+
+    print("ASD ", payload_bytes)
+
+    #Opening of cover file
+    cover_bytes = []
+    with open(file_to_hide_in, "rb") as out:
+        while True:
+            c = out.read(1)
+            if not c:
+                print("End of file")
+                break
+            cover_bytes.append(ord(c))
+    #print(cover_bytes)
+
 
     payload_size = os.path.getsize(file_to_hide)  # file_size is the same as len(file_bytes)
     print("The payload file size is ", payload_size)
@@ -112,10 +146,20 @@ def encodeTextFile(file_to_hide, file_to_hide_in, n_bits):
 
         data.append(char_hide + char_hide_in)
 
-    with open("encoded_file.txt", "w", encoding="utf-8") as f:
+    with open("encoded_file.docx", "w", encoding="utf-8") as f:
         for i in data:
             f.write(chr(i))
-    print(data)
+    #print(data)
+
+    """#Part for encoding into docx file. NOT WORKING
+    textList = ""
+    encoded_doc = docx.Document()
+    for i in data:
+        textList += chr(i)
+    
+    print(textList)
+    encoded_doc.add_paragraph(textList)
+    encoded_doc.save("encoded_file.docx")"""
     
     return None
 
@@ -137,33 +181,43 @@ def decodeFile(file_to_decode, n_bits):
 
         char_encoded = shift_n_bits_to_8(char_encoded, n_bits)
 
-        print(chr(char_encoded))
+        #print(chr(char_encoded))
         data += chr(char_encoded)
+
+    print(data)
 
     with open("decoded_file.txt", "w", encoding="utf-8") as f:
         f.write(data)
+
+    
+    #saveDocxFile(data, "decoded_file.docx")
+
     return None
 
 # # Running encode function
 # # no. of bits
-n_bits = 7
+n_bits = 8
 # # path of files replace them as per your need.
-#image_to_hide_path = "tree2.jpg"
-#image_to_hide_in_path = "test.txt"
-#image_to_hide_in_path = "wordTest.docx"
-#image_to_hide_in_path = "excel.xlsx"
+#txt to txt
+#file_to_hide_path = "payload_file.txt"
+#cover_file_path = "test.txt"
+
+#txt to docx
 file_to_hide_path = "payload_file.txt"
-cover_file_path = "test.txt"
-#image_to_hide = Image.open(cover_img_path)
-#height, width = encodeImage(image_to_hide, image_to_hide_in_path, n_bits)
+cover_file_path = "wordTest.docx"
+
+#docx to txt
+#file_to_hide_path = "payload_file_docx.docx"
+#cover_file_path = "test.txt"
 encodeTextFile(file_to_hide_path, cover_file_path, n_bits)
 print("File encoded Successfully!")
 #
 # # running the decode function
-n_bits = 7
+n_bits = 8
 #
 # # path where you would want to save decoded Image.
-decoded_file_path = "decoded_file.txt"
-file_to_decode_path = "encoded_file.txt"
+#file_to_decode_path = "encoded_file.docx"
+
+file_to_decode_path = "encoded_file.docx" #Change in line 149 too
 decodeFile(file_to_decode_path, n_bits)
 print("Image decoded Successfully!")
